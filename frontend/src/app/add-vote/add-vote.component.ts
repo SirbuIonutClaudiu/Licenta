88cc42682c43c895f5e9e5e7c89106054f17356d
SirbuIonutClaudiu <61731217+SirbuIonutClaudiu@users.noreply.github.com>
2021-05-30 02:26:27 +0300

    V.3.8.5

--- frontend/src/app/add-vote/add-vote.component.ts
+++ frontend/src/app/add-vote/add-vote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-add-vote',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-vote.component.css']
 })
 export class AddVoteComponent implements OnInit {
+  @ViewChild('test', {static: false}) test!: ElementRef;
+  duration_options = ['30 seconds', '1 minute', '2 minutes', '3 minutes', '4 minutes', '5 minutes'];
+  duration = 'Duration';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectDuration(option: string) {
+    this.duration = option;
+    this.test.nativeElement.click();
+    this.test.nativeElement.style.cssText += 'color: #556B2F;';
   }
 
 }
