a0ce977c43837b6b91734f2f3f7e77c04a679b82
SirbuIonutClaudiu <61731217+SirbuIonutClaudiu@users.noreply.github.com>
2021-05-31 21:37:03 +0300

    V.3.8.5.5

--- frontend/src/app/add-vote/add-vote.component.ts
+++ frontend/src/app/add-vote/add-vote.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {UserService} from '../_services/user.service';
 
 @Component({
   selector: 'app-add-vote',
@@ -7,12 +8,30 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 })
 export class AddVoteComponent implements OnInit {
   @ViewChild('test', {static: false}) test!: ElementRef;
+  maxLength = 10;
+  minLength = 5;
   duration_options = ['30 seconds', '1 minute', '2 minutes', '3 minutes', '4 minutes', '5 minutes'];
   duration = 'Duration';
+  subject = '';
+  subjectError = false;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.addInputMaxLength();
+  }
+
+  addInputMaxLength(): void {
+    document.addEventListener('keydown', (evt) => {
+      const subject = document.getElementById('subject');
+      if ((evt.target === subject) && (this.subject.length === this.maxLength)) {
+        this.subjectError = true;
+        this.userService.sleep(3).subscribe(
+          ans => {
+            this.subjectError = false;
+          });
+      }
+    });
   }
 
   selectDuration(option: string) {
